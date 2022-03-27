21950c15b53dcdf1c9c35de4cb108639af7a19aa
avs-abhishek123 <abhishek.avs123@gmail.com>
2022-03-27 21:19:46 +0530

    corrected the code for sum and multiplication function

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,17 @@
 print("adding all the data processing code is here  ")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return c
+
+print("sum function")
+
+def multiplication(a,b):
+    d =a*b
+    return d
+
+print("Multiplication function")
+    
