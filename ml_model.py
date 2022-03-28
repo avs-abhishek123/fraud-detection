a48da37fc24ded7c2eee7ec891d4a7602f2ac431
avs-abhishek123 <abhishek.avs123@gmail.com>
2022-03-29 01:41:33 +0530

    model2 is the final model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("machine leearning models")
+
+def model1():
+    print("adding code to ML moidel 1")
+    
+def model1():
+    print("adding code to ML moidel 1")
+    
+def model1():
+    print("adding code to ML moidel 1")        
+    
+print("ML model 2 performs the best")
